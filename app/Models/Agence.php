2d284d5d15f1e59f0b50f75b9afbf82947c82c68
anonymyoyo <paulemmanuel@gmail.com>
2024-10-29 17:23:08 -0700

    user profile

--- app/Models/Agence.php
+++ app/Models/Agence.php
@@ -2,10 +2,38 @@
 
 namespace App\Models;
 
+use App\Models\Association;
+use App\Models\User;
+use App\Models\Zone;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Agence extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'ville',
+        'user_id',
+        'pays',
+        'image',
+    ];
+
+
+    public function associations(): HasOne{
+        return $this->hasOne(Association::class);
+    }
+
+    public function user():HasOne{
+        return $this->hasOne(User::class);
+    }
+
+    public function zones():HasMany{
+        return $this->hasMany(Zone::class);
+    }
 }
--- app/Models/ChefAgence.php
+++ app/Models/ChefAgence.php
@@ -2,10 +2,35 @@
 
 namespace App\Models;
 
+use App\Models\Association;
+use App\Models\User;
+use App\Models\Zone;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class ChefAgence extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'agence_id',
+        'association_id',
+        'image',
+    ];
+
+
+    public function associations(): HasOne{
+        return $this->hasOne(Association::class);
+    }
+
+    public function user():HasOne{
+        return $this->hasOne(User::class);
+    }
+
+    public function zones():HasMany{
+        return $this->hasMany(Zone::class);
+    }
 }
